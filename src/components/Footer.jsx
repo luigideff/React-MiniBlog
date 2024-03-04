805e36dcb23aba27ef9dcfed22eb7a300bf72eaa
Luigi de Franceschi Fiamoncini <luigideff@outlook.com>
2024-03-04 09:15:34 -0300

    Added Montserrat font and some global CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Write about your interests!</h3>
       <p>Mini Blog &copy; 2024</p>
     </footer>
