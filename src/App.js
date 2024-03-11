ee3286c057fe96acc50bc2403d4735bf2c1ad144
Luigi de Franceschi Fiamoncini <luigideff@outlook.com>
2024-03-11 15:25:45 -0300

    Creating search functionality

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useState, useEffect } from 'react';
 import { useAuthentication } from './hooks/useAuthentication';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
 
 function App() {
 
@@ -38,6 +39,7 @@ if (loadingUser) {
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
+            <Route path='/search' element={<Search/>}/>
             <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
             <Route path='/register' element={!user ? <Register/> : <Navigate to="/"/>}/>
             <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to="/login"/>}/>
