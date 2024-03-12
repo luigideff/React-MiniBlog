a0810d588f6e46ed4ea44db478896835cf5f4ef0
Luigi de Franceschi Fiamoncini <luigideff@outlook.com>
2024-03-12 11:01:16 -0300

    Load posts into dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -8,8 +8,7 @@ const Dashboard = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
 
-  // Posts do usuário
-  const posts = [];
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
   return (
     <div>
@@ -27,6 +26,7 @@ const Dashboard = () => {
           <p>Tem posts</p>
         </div>
       )}
+      {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
   );
 };
